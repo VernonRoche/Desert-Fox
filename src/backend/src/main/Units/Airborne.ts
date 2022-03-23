76f86f12f0f2f83490a3abd9e4eb44196df855d4
Stergiou Alexandros <alexandros.stergiou@etu.u-bordeaux.fr>
2022-03-23 16:53:06 +0100

    Ajout des unités dans un dossier séparé

---
+++ src/backend/src/main/Units/Airborne.ts
@@ -0,0 +1,29 @@
+import AbstractUnit from "./AbstractUnit";
+import HexID from "./HexID";
+
+export default class Airborne extends AbstractUnit {
+  refit(): void {
+    throw new Error("Method not implemented.");
+  }
+  train(): void {
+    throw new Error("Method not implemented.");
+  }
+  reactionMove(hexId: HexID): void {
+    throw new Error("Method not implemented.");
+  }
+  moraleCheck(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  overrun(hexId: HexID): void {
+    throw new Error("Method not implemented.");
+  }
+  hasGeneralSupply(): boolean {
+    throw new Error("Method not implemented.");
+  }
+  attack(hexId: HexID, combatSupply: boolean): void {
+    throw new Error("Method not implemented.");
+  }
+  getId(): number {
+    throw new Error("Method not implemented.");
+  }
+}
