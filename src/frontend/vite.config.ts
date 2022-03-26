4250a90b9eba8eb619d58797b61684166eb6f9c0
Garion Goubard <garion.goubard@etu.u-bordeaux.fr>
2022-03-26 18:16:44 +0100

    replaced vue config with vite

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 8000,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:3000', // redirect to good port
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
 })
