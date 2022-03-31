43d085db7db444d6a76b371b8171f36e6f8c89ee
kdhima <kristo.dhima@etu.u-bordeaux.fr>
2022-03-31 16:36:16 +0200

    Implementation of Terrains and Map Types

--- src/backend/src/main/Map/Terrain.ts
+++ src/backend/src/main/Map/Terrain.ts
@@ -1,1 +1,20 @@
-export default class Terrain {}
+
+export enum TerrainTypes {
+        CLEAR = "clear",
+        ROUGH = "rough",
+        OASIS = "oasis",
+        SAND_SEA = "sand sea",
+        CITY = "city",
+        MARSH = "marsh",
+        EMPTY = "empty",
+    }
+
+
+export default class Terrain {
+    private _terrainType: TerrainTypes;
+
+    constructor(public terrainType: TerrainTypes) {
+        this._terrainType = terrainType;
+    }
+
+}
