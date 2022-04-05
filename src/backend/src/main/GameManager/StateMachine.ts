d938990a35be99dd5e2e370795fdc35bc90ad601
kdhima <kristo.dhima@etu.u-bordeaux.fr>
2022-04-05 16:58:28 +0200

    Modification of state machine(still testing)

--- src/backend/src/main/GameManager/StateMachine.ts
+++ src/backend/src/main/GameManager/StateMachine.ts
@@ -2,94 +2,89 @@ import { createMachine , interpret } from 'xstate';
 import webSocketServer, { SocketServer } from '../SocketServer';
 import Phase, { Turn } from './Turn';
 
+const statesWithUserInput = {
+    reinforcements: {
+        on: {
+            NEXT : 'allocation'
+        }
+    },
+    initiative: {
+        on: {
+            NEXT : 'first_player_movement'
+        }
+    },
+    allocation: {
+        on: {
+            NEXT : 'initiative'
+        }
+    },
+    first_player_movement: {
+        on: {
+            NEXT : 'second_player_reaction'
+        }
+    },
+    second_player_reaction: {
+        on: {
+            NEXT : 'first_player_combat'
+        }
+    },
+    first_player_combat: {
+        on: {
+            NEXT : 'second_player_movement'
+        }
+    },
+    second_player_movement: {
+        on: {
+            NEXT : 'first_player_reaction'
+        }
+    },
+    first_player_reaction: {
+        on: {
+            NEXT : 'second_player_combat'
+        }
+    },
+    second_player_combat: {
+        on: {
+            NEXT : 'first_player_movement2'
+        }
+    },
+    first_player_movement2: {
+        on: {
+            NEXT : 'second_player_reaction2'
+        }
+    },
+    second_player_reaction2: {
+        on: {
+            NEXT : 'first_player_combat2'
+        }
+    },
+    first_player_combat2: {
+        on: {
+            NEXT : 'second_player_movement2'
+        }
+    },
+    second_player_movement2: {
+        on: {
+            NEXT : 'first_player_reaction2'
+        }
+    },
+    first_player_reaction2: {
+        on: {
+            NEXT : 'second_player_combat2'
+        }
+    },
+    second_player_combat2: {
+        on: {
+            NEXT : 'supply_attrition'
+        }
+    },
+
+
+}
 const TurnPhases = {
     initial: 'event',
     states: {
-        event: {
-            on: {
-                NEXT : 'air_superiority'
-            }
-        },
-        air_superiority: {
-            on: {
-                NEXT : 'reinforcements'
-            }
-        },
-        reinforcements: {
-            on: {
-                NEXT : 'allocation'
-            }
-        },
-        allocation: {
-            on: {
-                NEXT : 'initiative'
-            }
-        },
-        initiative: {
-            on: {
-                NEXT : 'first_player_movement'
-            }
-        },
-        first_player_movement: {
-            on: {
-                NEXT : 'second_player_reaction'
-            }
-        },
-        second_player_reaction: {
-            on: {
-                NEXT : 'first_player_combat'
-            }
-        },
-        first_player_combat: {
-            on: {
-                NEXT : 'second_player_movement'
-            }
-        },
-        second_player_movement: {
-            on: {
-                NEXT : 'first_player_reaction'
-            }
-        },
-        first_player_reaction: {
-            on: {
-                NEXT : 'second_player_combat'
-            }
-        },
-        second_player_combat: {
-            on: {
-                NEXT : 'first_player_movement2'
-            }
-        },
-        first_player_movement2: {
-            on: {
-                NEXT : 'second_player_reaction2'
-            }
-        },
-        second_player_reaction2: {
-            on: {
-                NEXT : 'first_player_combat2'
-            }
-        },
-        first_player_combat2: {
-            on: {
-                NEXT : 'second_player_movement2'
-            }
-        },
-        second_player_movement2: {
-            on: {
-                NEXT : 'first_player_reaction2'
-            }
-        },
-        first_player_reaction2: {
-            on: {
-                NEXT : 'second_player_combat2'
-            }
-        },
-        second_player_combat2: {
-            on: {
-                NEXT : 'supply_attrition'
-            }
-        },
+        ...statesWithUserInput,
         supply_attrition: {
             on: {
                 NEXT : 'victory_check'
@@ -124,7 +119,9 @@ export default phaseService;
 webSocketServer.sockets.forEach((socket) => {
     socket.addListener("nextphase", () => {
         phaseService.send('NEXT');
+        
         socket.emit("phase", phaseService.state.value);
+        
         
         });
 });
