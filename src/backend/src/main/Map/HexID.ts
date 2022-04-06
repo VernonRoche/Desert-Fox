c7fd4ac50c34d24b51ddcd5500a024b28a4c8392
Kristo Dhima <kristo.dhima@etu.u-bordeaux.fr>
2022-04-06 16:47:17 +0200

    fixed hexId

--- src/backend/src/main/Map/HexID.ts
+++ src/backend/src/main/Map/HexID.ts
@@ -10,8 +10,7 @@ export default class HexID {
     function zeroIfNeeded(num: number) {
       return (num < 10 ? "0" : "") + num;
     }
-    const id = zeroIfNeeded(this._x) + zeroIfNeeded(this._y);
-    return id;
+    return zeroIfNeeded(this._y) + zeroIfNeeded(this._x);
   }
 
   getX() {
