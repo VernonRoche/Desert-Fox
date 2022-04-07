6abdaf67922370b0030be90e2c2d79ea8aad772d
Alexandros Stergiou <alexandros.stergiou@etu.u-bordeaux.fr>
2022-04-07 18:01:11 +0200

    Converted "any" type of Pathfinder graph to Map<string, PathfinderNode>

--- src/backend/src/main/GameManager/Pathfinder.ts
+++ src/backend/src/main/GameManager/Pathfinder.ts
@@ -4,10 +4,10 @@ import Player from "./Player";
 import GameMap from "../Map/GameMap";
 
 class Pathfinder {
-  private _nodes: any;
+  private _nodes: Map<string, PathfinderNode>;
   private _map: GameMap;
   constructor(map: GameMap) {
-    this._nodes = {};
+    this._nodes = new Map();
     this._map = map;
     for (const hex of map.getHexes().values()) {
       const node: PathfinderNode = new PathfinderNode(hex.getID(), hex.getTerrain().getWeight());
@@ -16,12 +16,12 @@ class Pathfinder {
           new PathfinderNode(neighbour.getID(), neighbour.getTerrain().getWeight()),
         );
       }
-      this._nodes[hex.getID().toString()] = node;
+      this._nodes.set(hex.getID().toString(), node);
     }
   }
 
   addNode(node: PathfinderNode): void {
-    this._nodes[node.name.toString()] = node;
+    this._nodes.set(node.name.toString(), node);
   }
 
   findPointsOfShortestWay(start: HexID, finish: HexID): HexID[] {
@@ -30,10 +30,16 @@ class Pathfinder {
     while (nextNode !== start) {
       let minWeigth: number = Number.MAX_VALUE;
       let minNode: HexID = new HexID(-1, -1);
-      for (const i of this._nodes[nextNode.toString()].neighbourNodes) {
-        if (i.weight + this._nodes[i.name.toString()].weight < minWeigth) {
-          minWeigth = this._nodes[i.name.toString()].weight;
-          minNode = i.name;
+      const tmpNode = this._nodes.get(nextNode.toString());
+      if (tmpNode) {
+        for (const i of tmpNode.neighbourNodes) {
+          const neighbourNode = this._nodes.get(i.name.toString());
+          if (neighbourNode) {
+            if (i.weight + neighbourNode.weight < minWeigth) {
+              minWeigth = neighbourNode.weight;
+              minNode = i.name;
+            }
+          }
         }
       }
       arrayWithNode.push(minNode);
@@ -50,34 +56,51 @@ class Pathfinder {
     const nodes: any = {};
 
     for (const i in this._nodes) {
-      if (this._nodes[i].name === start) {
-        this._nodes[i].weight = 0;
-      } else {
-        this._nodes[i].weight = Number.MAX_VALUE;
+      const iNode = this._nodes.get(i);
+      if (iNode) {
+        if (iNode.name === start) {
+          iNode.weight = 0;
+        } else {
+          iNode.weight = Number.MAX_VALUE;
+        }
+        nodes[iNode.name.toString()] = iNode.weight;
       }
-      nodes[this._nodes[i].name] = this._nodes[i].weight;
-    }
 
-    while (Object.keys(nodes).length !== 0) {
-      const sortedVisitedByWeight: string[] = Object.keys(nodes).sort(
-        (a, b) => this._nodes[a].weight - this._nodes[b].weight,
-      );
-      const currentNode: PathfinderNode = this._nodes[sortedVisitedByWeight[0]];
-      for (const j of currentNode.neighbourNodes) {
-        let calculateWeight: number = currentNode.weight + j.weight;
-        if (!this._map.hexBelongsToPlayer(j.name, player)) {
-          calculateWeight += 1000;
+      while (Object.keys(nodes).length !== 0) {
+        const sortedVisitedByWeight: string[] = Object.keys(nodes).sort((a, b) => {
+          const aNode = this._nodes.get(a);
+          const bNode = this._nodes.get(b);
+          if (aNode && bNode) {
+            return aNode.weight - bNode.weight;
+          }
+          return -1;
+        });
+        const currentNode = this._nodes.get(sortedVisitedByWeight[0]);
+        if (currentNode) {
+          for (const j of currentNode.neighbourNodes) {
+            let calculateWeight: number = currentNode.weight + j.weight;
+            if (!this._map.hexBelongsToPlayer(j.name, player)) {
+              calculateWeight += 1000;
+            }
+            const jNode = this._nodes.get(j.name.toString());
+            if (jNode) {
+              if (calculateWeight < jNode.weight) {
+                jNode.weight = calculateWeight;
+              }
+            }
+          }
         }
-        if (calculateWeight < this._nodes[j.name.toString()].weight) {
-          this._nodes[j.name.toString()].weight = calculateWeight;
-        }
+        delete nodes[sortedVisitedByWeight[0]];
       }
-      delete nodes[sortedVisitedByWeight[0]];
+      const finishNode = this._nodes.get(finish.toString());
+      if (finishNode) {
+        const finishWeight: number = finishNode.weight;
+        const arrayWithVertex: HexID[] = this.findPointsOfShortestWay(start, finish).reverse();
+        arrayWithVertex.push(finish);
+        return { pathNodes: arrayWithVertex, weight: finishWeight };
+      }
     }
-    const finishWeight: number = this._nodes[finish.toString()].weight;
-    const arrayWithVertex: HexID[] = this.findPointsOfShortestWay(start, finish).reverse();
-    arrayWithVertex.push(finish);
-    return { pathNodes: arrayWithVertex, weight: finishWeight };
+    return { pathNodes: [], weight: -1 };
   }
 }
 
