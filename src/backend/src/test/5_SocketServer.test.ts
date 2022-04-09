794a7a7f46c25aa781174c684e6f7f8f6fcc328b
Garion Goubard <garion.goubard@etu.u-bordeaux.fr>
2022-04-09 18:33:51 +0200

    start test (will fail pipeline), added socket io client

---
+++ src/backend/src/test/5_SocketServer.test.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import { io, Socket } from "socket.io-client";
+import SocketServer from "../main/SocketServer";
+
+function initSocket(port: number): Socket {
+  return io(`http://localhost:${port}`);
+}
+
+describe("Socket server tests", function () {
+  let socketServer: SocketServer;
+  let player1: Socket;
+  let player2: Socket;
+  const CLIENT_PORT = 5050;
+  const SERVER_PORT = 5000;
+  describe("Initialisation", function () {
+    it("Initialize socket server", function () {
+      socketServer = new SocketServer(express(), CLIENT_PORT, SERVER_PORT);
+    });
+
+    it("webServer stores correctly clientPort", function () {
+      if (socketServer.clientPort !== CLIENT_PORT) {
+        throw new Error("clientPort is not stored correctly");
+      }
+    });
+
+    it("webServer stores correctly serverPort", function () {
+      if (socketServer.serverPort !== SERVER_PORT) {
+        throw new Error("serverPort is not stored correctly");
+      }
+    });
+  });
+
+  it("Run webServer", function () {
+    socketServer.run();
+  });
+
+  it("Initialize player 1 and connect", function () {
+    return new Promise<void>((resolve) => {
+      player1 = initSocket(CLIENT_PORT);
+      player1.on("connect", () => {
+        resolve();
+      });
+    });
+  });
+
+  it("Initialize player 2 and connect", function () {
+    return new Promise<void>((resolve) => {
+      player2 = initSocket(CLIENT_PORT);
+      player2.on("connect", () => {
+        resolve();
+      });
+    });
+  });
+});
