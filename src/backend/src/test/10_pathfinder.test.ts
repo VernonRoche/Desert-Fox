042e8150d4f87eb464613e58c079de5f51182916
Alexandros Stergiou <alexandros.stergiou@etu.u-bordeaux.fr>
2022-04-11 14:38:30 +0200

    New version of Pathfinder

--- src/backend/src/test/10_pathfinder.test.ts
+++ src/backend/src/test/10_pathfinder.test.ts
@@ -1,11 +1,11 @@
-/* import Game from "../main/GameManager/Game";
+/*import Game from "../main/GameManager/Game";
 import GameMap from "../main/Map/GameMap";
+import Mechanized from "../main/Units/Mechanized";
 import HexID from "../main/Map/HexID";
 import Maps from "../main/Map/Maps";
 import Player from "../main/GameManager/Player";
 import PlayerID from "../main/GameManager/PlayerID";
 import { Socket } from "socket.io";
-import Garrison from "../main/Units/Garrison";
 
 describe("Check if Pathfinder works correctly", function () {
   let game: Game;
@@ -13,8 +13,8 @@ describe("Check if Pathfinder works correctly", function () {
     const hexIDAlly = new HexID(5, 7);
     const hexIDEnemy = new HexID(5, 8);
 
-    const testUnitAlly = new Garrison(0, hexIDAlly, 15, 15, 1);
-    const testUnitEnemy = new Garrison(1, hexIDEnemy, 15, 15, 1);
+    const testUnitAlly = new Mechanized(0, hexIDAlly, 15, 15, 1);
+    const testUnitEnemy = new Mechanized(1, hexIDEnemy, 15, 15, 1);
 
     const allyPlayer = new Player(PlayerID.ONE, [testUnitAlly], [], [], [], Socket.prototype);
     const enemyPlayer = new Player(PlayerID.TWO, [testUnitEnemy], [], [], [], Socket.prototype);
@@ -24,44 +24,49 @@ describe("Check if Pathfinder works correctly", function () {
 
   it("The weight returned should be the movement points cost", function () {
     const pathfinder = game.getPathfinder();
-    const { weight } = pathfinder.findShortestWay(
-      new HexID(5, 7),
-      new HexID(5, 9),
+    const { sumOfWeight } = pathfinder.findShortestWay(
+      game.getMap().findHex(new HexID(5, 7)),
+      game.getMap().findHex(new HexID(5, 9)),
       game.getPlayer1(),
+      false,
     );
-    if (weight !== 3) {
+    if (sumOfWeight !== 3) {
       throw new Error(
-        "The weight returned is different from the movement cost. The returned weight is " + weight,
+        "The weight returned is different from the movement cost. The returned weight is " +
+          sumOfWeight,
       );
     }
   });
 
   it("It finds the shortest path", function () {
     const pathfinder = game.getPathfinder();
-    const { weight } = pathfinder.findShortestWay(
-      new HexID(5, 7),
-      new HexID(5, 9),
+    const { sumOfWeight } = pathfinder.findShortestWay(
+      game.getMap().findHex(new HexID(5, 7)),
+      game.getMap().findHex(new HexID(5, 9)),
       game.getPlayer1(),
+      false,
     );
-    if (weight > 3) {
+    if (sumOfWeight > 3) {
       throw new Error(
-        "The pathfinder did not find the optimal path with a cost of 3, instead it's " + weight,
+        "The pathfinder did not find the optimal path with a cost of 3, instead it's " +
+          sumOfWeight,
       );
     }
   });
 
   it("Does not go through or enter enemy hex", function () {
     const pathfinder = game.getPathfinder();
-    const { pathNodes } = pathfinder.findShortestWay(
-      new HexID(5, 7),
-      new HexID(5, 9),
+    const { hexPath } = pathfinder.findShortestWay(
+      game.getMap().findHex(new HexID(5, 7)),
+      game.getMap().findHex(new HexID(5, 9)),
       game.getPlayer1(),
+      false,
     );
-    for (const node of pathNodes) {
+    for (const node of hexPath) {
       if (node.toString() === new HexID(5, 8).toString()) {
         throw new Error("The pathfinder goes through enemy hex");
       }
     }
   });
 });
- */
+*/
