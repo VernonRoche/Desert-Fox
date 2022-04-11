8988e3576c5f62bfd0ec7770cab0bd313e7624b5
Garion Goubard <garion.goubard@etu.u-bordeaux.fr>
2022-04-11 21:49:43 +0200

    fixed order in gamebar

--- src/frontend/src/components/GameBar.vue
+++ src/frontend/src/components/GameBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gradient-to-bl from-blue-300 to-violet-700 p-2 text-white font-bold">
     <div v-if="gameCreated" class="flex justify-around items-center">
-      <h1>Phase actuelle: {{ phaseSentence }} | {{ phase }}</h1>
+      <h1>Phase actuelle: {{ phaseSentence }}</h1>
       <h1>{{ turnSentence }}</h1>
       <h1>Nombre d'unités restantes: {nbUnits}</h1>
     </div>
@@ -10,19 +10,20 @@
 </template>
 
 <script lang="ts" setup>
-const gameCreated = ref(false);
 import { computed, ref } from "vue";
 import socket from "../utils/ClientSocket";
-socket.on("map", () => {
-  gameCreated.value = true;
-});
 
 const phases: Record<string, string> = {
   player_movement: "Phase de déplacement",
 };
 
+const gameCreated = ref(false);
 const phase = ref("");
 const isPlaying = ref(false);
+
+socket.on("map", () => {
+  gameCreated.value = true;
+});
 
 const phaseSentence = computed(() => {
   const fullName = phases[phase.value];
