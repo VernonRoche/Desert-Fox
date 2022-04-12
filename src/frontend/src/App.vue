00246cdf300607bfb6f35eb1e442956ec8680d1b
Garion Goubard <garion.goubard@etu.u-bordeaux.fr>
2022-04-12 16:38:35 +0200

    fix

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -5,17 +5,3 @@
     </main>
   </div>
 </template>
-
-<script lang="ts" setup>
-import NavBar from "./components/NavBar.vue";
-</script>
-
-<style scoped>
-nav a.router-link-exact-active {
-  @apply underline;
-}
-
-.fullscreen {
-  @apply h-screen w-screen;
-}
-</style>
