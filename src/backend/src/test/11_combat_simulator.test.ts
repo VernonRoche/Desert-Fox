337ed7e793c32407a745dac0aa64e47d535ae958
Alexandros Stergiou <alexandros.stergiou@etu.u-bordeaux.fr>
2022-04-15 15:47:29 +0200

    Name fix in combat test

--- src/backend/src/test/11_combat_simulator.test.ts
+++ src/backend/src/test/11_combat_simulator.test.ts
@@ -1,7 +1,7 @@
 import { TerrainTypes } from "../main/Map/Terrain";
 import CombatSimulator, { DamageResult, MoraleResult } from "../main/GameManager/CombatSimulator";
 
-describe("Check if Pathfinder works correctly", function () {
+describe("Check if combat simulator works correctly", function () {
   it("Morale results should be correct", function () {
     let morale = 4;
     let terrain = TerrainTypes.ROUGH;
