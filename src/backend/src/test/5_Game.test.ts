c5fd242b07c2751f468c4844059ecaccfb237dd2
Garion Goubard <garion.goubard@etu.u-bordeaux.fr>
2022-04-16 21:14:31 +0200

    game tests work now

--- src/backend/src/test/5_Game.test.ts
+++ src/backend/src/test/5_Game.test.ts
@@ -1,88 +1,147 @@
-import express from "express";
 import { io, Socket } from "socket.io-client";
-import { StateMachine } from "../main/GameManager/StateMachine";
-import { SocketServer } from "../main/SocketServer";
+import stateMachine from "../main/GameManager/StateMachine";
+import webSocketServer, { SocketServer } from "../main/SocketServer";
 import AbstractUnit from "../main/Units/AbstractUnit";
 
 function closeServer(server: SocketServer) {
   server["_httpServer"].close();
 }
 
-function initSocket(port: number): Socket {
+function initSocket(port = 3001): Socket {
   return io(`http://localhost:${port}`);
 }
 
 describe("Game tests", function () {
-  function testForBothPlayers(
-    title: (playerName: string) => string,
-    test: (playerSocket: Socket, playerName: string) => void,
-  ) {
-    it(title("player1"), function () {
-      test(player1, "player2");
+  this.timeout(500);
+  let player1: Socket;
+  let player2: Socket;
+
+  webSocketServer.run(stateMachine);
+
+  it("Initialize player1", async function () {
+    return new Promise<void>((resolve) => {
+      player1 = initSocket();
+      player1.once("connect", () => {
+        console.log("Player 1 connected!");
+        resolve();
+      });
     });
-    it(title("player2"), function () {
-      test(player2, "player2");
-    });
-  }
-  let socketServer: SocketServer;
-  const CLIENT_PORT = 5051;
-  const SERVER_PORT = 5001;
-  const player1 = initSocket(SERVER_PORT);
-  const player2 = initSocket(SERVER_PORT);
-  let stateMachine: StateMachine;
-  it("Initialize socket server", function () {
-    socketServer = new SocketServer(express(), CLIENT_PORT, SERVER_PORT, false);
-    stateMachine = new StateMachine(false);
-    socketServer.run(stateMachine);
   });
 
-  testForBothPlayers(
-    (playerName) => `Initialize ${playerName} and connect`,
-    (playerSocket) => {
-      return new Promise<void>((resolve) => {
-        playerSocket.on("connect", () => {
-          resolve();
-        });
+  it("Initialize player2", async function () {
+    return new Promise<void>((resolve) => {
+      player2 = initSocket();
+      player2.once("connect", () => {
+        console.log("Player 2 connected!");
+        resolve();
       });
-    },
-  );
+    });
+  });
 
-  it("Game should be started", function () {
-    if (!socketServer.getGame()) {
-      throw new Error("Game is not started with 2 players");
+  it("Game should be started", async function () {
+    if (!webSocketServer.getGame()) {
+      throw new Error(
+        "Game is not started with 2 players " +
+          player1.connected +
+          " " +
+          player2.connected +
+          " " +
+          webSocketServer.getGame(),
+      );
     }
   });
 
-  testForBothPlayers(
-    (playerName) => `get ${playerName} units`,
-    (playerSocket, playerName) => {
-      return new Promise<void>((resolve, reject) => {
-        playerSocket.emit("command", { type: "units" });
-        playerSocket.on("units", (units: AbstractUnit[] & { error: string }) => {
-          if (units.error) {
-            throw new Error("No units, got: " + units.error);
-          }
-          if (units.length !== 6) {
-            reject(new Error(`${playerName} has not 6 units but ` + units.length));
-          }
-          resolve();
-        });
+  it("get player1 units", async function () {
+    return new Promise<void>((resolve, reject) => {
+      player1.emit("command", { type: "units" });
+      player1.on("units", (units: AbstractUnit[] & { error: string }) => {
+        if (units.error) {
+          throw new Error("No units, got: " + units.error);
+        }
+        if (units.length !== 6) {
+          reject(new Error(`player1 has not 6 units but ` + units.length));
+        }
+        resolve();
       });
-    },
-  );
+    });
+  });
+
+  it("get player2 units", async function () {
+    return new Promise<void>((resolve, reject) => {
+      player2.emit("command", { type: "units" });
+      player2.on("units", (units: AbstractUnit[] & { error: string }) => {
+        if (units.error) {
+          throw new Error("No units, got: " + units.error);
+        }
+        if (units.length !== 6) {
+          reject(new Error(`player2 has not 6 units but ` + units.length));
+        }
+        resolve();
+      });
+    });
+  });
 
   it("First player moves a valid unit", function () {
     return new Promise<void>((resolve, reject) => {
-      player1.emit("command", { type: "move", unitId: "0", hexId: "0102" });
+      player1.emit("command", { type: "move", unitId: "0", hexId: "0103" });
       player1.on("move", (resp: { error: string | false }) => {
         if (resp.error) {
+          if (resp.error === "invalidturncommand") {
+            reject(
+              new Error(
+                "Invalid turn command, phase is " +
+                  stateMachine.getPhaseService().state.value.toString(),
+              ),
+            );
+          }
           reject(new Error("Move error: " + resp.error));
         }
         resolve();
       });
     });
   });
+
+  it("First player fails to move an unexisting unit", function () {
+    return new Promise<void>((resolve, reject) => {
+      player1.emit("command", { type: "move", unitId: "1000", hexId: "0103" });
+      player1.on("move", (resp: { error: string | false }) => {
+        if (resp.error) {
+          resolve();
+        }
+        reject(new Error("Player cannot move this unit"));
+      });
+    });
+  });
+
+  it("skip to next player", function () {
+    player1.emit("done");
+    player2.emit("done");
+    player1.emit("done");
+  });
+
+  describe("Second player movement", function () {
+    it("Second player moves a valid unit", function () {
+      return new Promise<void>((resolve, reject) => {
+        player2.emit("command", { type: "move", unitId: "6", hexId: "2031" });
+        player2.on("move", (resp: { error: string | false }) => {
+          if (resp.error) {
+            if (resp.error === "invalidturncommand") {
+              reject(
+                new Error(
+                  "Invalid turn command, phase is " +
+                    stateMachine.getPhaseService().state.value.toString(),
+                ),
+              );
+            }
+            reject(new Error("Move error:" + resp.error));
+          }
+          resolve();
+        });
+      });
+    });
+  });
+
   it("Close server", function () {
-    closeServer(socketServer);
+    closeServer(webSocketServer);
   });
 });
